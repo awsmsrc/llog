fb3d941c6c1a077d868feb61423897387589c65b
Luke Roberts <email@luke-roberts.co.uk>
2014-07-09 21:07:36 +0100

    variadic f functions

--- llog.go
+++ llog.go
@@ -1,18 +1,18 @@
 package llog
 
 import (
+  "fmt"
 	"errors"
-	"fmt"
-	"os"
-	"time"
+  "os"
+  "time"
 )
 
 const (
-	RED     = "\033[31m"
-	GREEN   = "\033[32m"
-	YELLOW  = "\033[93m"
-	CYAN    = "\033[36m"
-	MAGENTA = "\033[95m"
+  RED = "\033[31m"
+  GREEN = "\033[32m"
+  YELLOW = "\033[93m"
+  CYAN = "\033[36m"
+  MAGENTA = "\033[95m"
 )
 
 const (
@@ -24,17 +24,27 @@ const (
 )
 
 var (
-	level int = 1
+  level int = 1
 )
 
-func coloredLog(color string, s string) {
-	fmt.Fprintf(
-		os.Stdout,
-		"%v[%v] %v\n",
-		color,
-		time.Now().Format("2006-01-02  15:04:05"),
-		s,
-	)
+func coloredLog (color string, s string) {
+  fmt.Fprintf(
+    os.Stdout, 
+    "%v[%v] %v\n", 
+    color, 
+    time.Now().Format("2006-01-02  15:04:05"), 
+    s,
+  )
+}
+
+func coloredLogF (color string, s string, a ...interface{}) {
+  fmt.Fprintf(
+    os.Stdout, 
+    "%v[%v] %v\n", 
+    color, 
+    time.Now().Format("2006-01-02  15:04:05"), 
+    fmt.Sprintf(s, a...),
+  )
 }
 
 func SetLevel(i int) error {
@@ -45,31 +55,61 @@ func SetLevel(i int) error {
 	return nil
 }
 
-func Debug(s string) {
+func Debug (s string) {
 	if level > LevelDebug || level == LevelNull {
 		return
 	}
-	coloredLog(YELLOW, s)
+  coloredLog(YELLOW, s)
 }
 
-func Info(s string) {
+func FDebug (s string, a ...interface{}) {
+	if level > LevelDebug || level == LevelNull {
+		return
+	}
+  coloredLogF(YELLOW, s, a)
+}
+
+func Info (s string) {
 	if level > LevelInfo || level == LevelNull {
 		return
 	}
-	coloredLog(CYAN, s)
+  coloredLog(CYAN, s)
 }
 
-func Warn(s string) {
+func FInfo (s string, a ...interface{}) {
+	if level > LevelInfo || level == LevelNull {
+		return
+	}
+  coloredLogF(CYAN, s, a)
+}
+
+func Warn (s string) {
 	if level > LevelWarn || level == LevelNull {
 		return
 	}
-	coloredLog(MAGENTA, s)
+  coloredLog(MAGENTA, s)
 }
 
-func Error(s string) {
-	coloredLog(RED, s)
+func FWarn (s string, a ...interface{}) {
+	if level > LevelWarn || level == LevelNull {
+		return
+	}
+  coloredLogF(MAGENTA, s, a)
 }
 
-func Success(s string) {
-	coloredLog(GREEN, s)
+func Error (s string) {
+  coloredLog(RED, s)
 }
+
+func FError (s string, a ...interface{}) {
+  coloredLogF(RED, s, a)
+}
+
+func Success (s string) {
+  coloredLog(GREEN, s)
+}
+
+func FSuccess (s string, a ...interface{}) {
+  coloredLogF(GREEN, s, a)
+}
+
