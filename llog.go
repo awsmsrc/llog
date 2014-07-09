ac75d8379126e36427f4baad03ae1a4c7be0d197
Luke Roberts <email@luke-roberts.co.uk>
2014-07-09 20:44:18 +0100

    initial commit

---
+++ llog.go
@@ -0,0 +1,76 @@
+package llog
+
+import (
+  "fmt"
+	"errors"
+  "os"
+  "time"
+)
+
+const (
+  RED = "\033[31m"
+  GREEN = "\033[32m"
+  YELLOW = "\033[93m"
+  CYAN = "\033[36m"
+  MAGENTA = "\033[95m"
+)
+
+const (
+	LevelNull = iota
+	LevelDebug
+	LevelInfo
+	LevelWarn
+	LevelError
+)
+
+var (
+  level int = 1
+)
+
+func coloredLog (color string, s string) {
+  fmt.Fprintf(
+    os.Stdout, 
+    "%v[%v] %v\n", 
+    color, 
+    time.Now().Format("2006-01-02  15:04:05"), 
+    s,
+  )
+}
+
+func SetLevel(i int) error {
+	if i < 1 || i > 4 {
+		return errors.New("log level out of range")
+	}
+	level = i
+	return nil
+}
+
+func Debug (s string) {
+	if level > LevelDebug || level == LevelNull {
+		return
+	}
+  coloredLog(YELLOW, s)
+}
+
+func Info (s string) {
+	if level > LevelInfo || level == LevelNull {
+		return
+	}
+  coloredLog(CYAN, s)
+}
+
+func Warn (s string) {
+	if level > LevelWarn || level == LevelNull {
+		return
+	}
+  coloredLog(MAGENTA, s)
+}
+
+func Error (s string) {
+  coloredLog(RED, s)
+}
+
+func Success (s string) {
+  coloredLog(GREEN, s)
+}
+
